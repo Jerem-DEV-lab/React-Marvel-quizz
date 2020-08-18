8c76634800335ab258f73c61ce41ac442b8164f3
Jeremy Guillemet <guillemet.jeremy087@gmail.com>
2020-08-18 15:23:05 +0200

    InProgress: :construction: 0.1 Alpha
    * :bookmark: release ErrorPage : 0.1

--- src/components/ErrorPage/index.js
+++ src/components/ErrorPage/index.js
@@ -1,10 +1,21 @@
 import React from "react";
+import batman from "../../images/batman.png"
 
-const ErrorPage = () => {
+const centerH2 = {
+    textAlign: 'center',
+    marginTop: '50px'
+}
+const resizeImg = {
+    display: 'block',
+    width: '50%',
+    margin: '40px auto'
+}
+const ErrorPage = (props) => {
     return (
         <div className="quiz-bg">
             <div className="container">
-                ErrorPage
+                <h2 style={centerH2}>Oups, cette page n'existe pas</h2>
+                <img style={resizeImg} src={batman} alt="batman"/>
             </div>
         </div>
     )
