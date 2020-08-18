03f286f299a1defe45c09a115d67527b0402705c
Jeremy Guillemet <guillemet.jeremy087@gmail.com>
2020-08-18 14:33:54 +0200

    Update: :sparkles: Ajout de composants
    * 🚧 Login
    * 🚧 SignUp
    * 🚧 ErrorPage

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import '../../App.css';
 import Header from "../Header";
 import Landing from "../Landing";
 import Footer from "../Footer";
+import Welcome from "../Welcome";
+import Login from "../Login";
+import SignUp from "../SignUp";
+import ErrorPage from "../ErrorPage";
+import '../../App.css';
+
 
 function Index() {
   return (
     <div>
       <Header />
-      <Landing />
-      <Footer/>
+
+        <Welcome/>
+        <Landing />
+        <Login />
+        <SignUp />
+        <Footer/>
+        <ErrorPage />
     </div>
   );
 }
