318bf905d404be4b89063cdb7de97dea7119058c
Jeremy Guillemet <guillemet.jeremy087@gmail.com>
2020-08-18 14:20:56 +0200

    Update: :dizzy: Landing ajout d'une animation
    * UI: Mise en place de onMouseOver & onMouseOut

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -14,12 +14,27 @@ const Landing = () => {
         }, 1000)
     }, [])
 
+    const setLeftImg = e => {
+        refWolverine.current.classList.add("leftImg");
+    }
+
+    const setRightImg = e => {
+            refWolverine.current.classList.add("rightImg");
+    }
+
+    const clearImg = () => {
+            if(refWolverine.current.classList.contains("leftImg")){
+                refWolverine.current.classList.remove('leftImg')
+            } else if (refWolverine.current.classList.contains("rightImg")) {
+                refWolverine.current.classList.remove('rightImg')
+        }
+    }
     const displayBtn = btn && (
         <Fragment>
-            <div className="leftBox">
+            <div className="leftBox" onMouseOver={setLeftImg} onMouseOut={clearImg}>
                 <button className="btn-welcome">Inscription</button>
             </div>
-            <div className="rightBox">
+            <div className="rightBox" onMouseOver={setRightImg} onMouseOut={clearImg}>
                 <button className="btn-welcome">Connexion</button>
             </div>
         </Fragment>
